31e1b6b5af6e7b259a9a4fb4fb5b2580f62b3108
Matt Post <mattpost@amazon.com>
2017-11-01 15:07:12 +0100

    reverted switch to bc, using local sacrebleu

--- test.sh
+++ test.sh
@@ -18,11 +18,11 @@ fi
 [[ ! -d data ]] && mkdir data
 cd data
 
-#if [[ ! -d wmt17-submitted-data ]]; then
-#    echo "Downloading and unpacking WMT'17 system submissions (46 MB)..."
-#    wget -q http://data.statmt.org/wmt17/translation-task/wmt17-submitted-data-v1.0.tgz
-#    tar xzf wmt17-submitted-data-v1.0.tgz
-#fi
+if [[ ! -d wmt17-submitted-data ]]; then
+   echo "Downloading and unpacking WMT'17 system submissions (46 MB)..."
+   wget -q http://data.statmt.org/wmt17/translation-task/wmt17-submitted-data-v1.0.tgz
+   tar xzf wmt17-submitted-data-v1.0.tgz
+fi
 
 for pair in cs-en de-en en-cs en-de en-fi en-lv en-ru en-tr fi-en lv-en ru-en tr-en zh-en; do
     tokenizer=13a
@@ -38,8 +38,8 @@ for pair in cs-en de-en en-cs en-de en-fi en-lv en-ru en-tr fi-en lv-en ru-en tr
         ref=wmt17-submitted-data/sgm/references/newstest2017-$source$target-ref.$target.sgm
 
         bleu1=$($MOSES/scripts/generic/mteval-v13a.pl -c -s $src -r $ref -t $sgm 2> /dev/null | grep "BLEU score" | cut -d' ' -f9)
-        bleu1=$(echo "${bleu1} * 100" | bc)
-        bleu2=$(cat $txt | sacrebleu -t wmt17 -l $source-$target | cut -d' ' -f3)
+        bleu1=$(echo "print($bleu1 * 100)" | python)
+        bleu2=$(cat $txt | ../sacrebleu.py -t wmt17 -l $source-$target | cut -d' ' -f3)
 
         echo "$source-$target $sys mteval: $bleu1 sacreBLEU: $bleu2"
     done
