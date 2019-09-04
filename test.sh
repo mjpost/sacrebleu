2b0909e8a0405371fe772abb4ac52e3570a869a4
Martin Popel <popel@ufal.mff.cuni.cz>
2019-09-04 13:16:14 -0700

    prevent non-printable characters in source codes
    test.sh included a literal carriage return character
    (for the purpose of testing that it does not affect BLEU).
    However, editing test.sh in some editors (including GitHub's merge function)
    either deletes the CR symbol or substitutes with LF.
    Thus, it is safer to print CR with `printf \r`.

--- test.sh
+++ test.sh
@@ -121,10 +121,9 @@ for pair in cs-en de-en en-cs en-de en-fi en-lv en-ru en-tr en-zh fi-en lv-en ru
     done
 done
 
-score1=$( echo "Hello! How are you doing today?" | ../sacrebleu.py -w 2 -b <(echo "Hello! How are you 
- doing today?") )
+score1=$( echo "Hello! How are you doing today?" | ../sacrebleu.py -w 2 -b <(printf "Hello! How are you \r doing today?") )
 score2=$( echo "Hello! How are you doing today?" | ../sacrebleu.py -w 2 -b <(echo "Hello! How are you doing today?") )
-if [[ $score1 != $score2 ]]; then 
+if [[ $score1 != $score2 ]]; then
   echo "Control character in reference test failed"
   exit 1
 fi
