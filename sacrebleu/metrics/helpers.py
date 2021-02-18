418143ff5b37c19d6668c4cbfe233668a0cbe6d3
Ozan Caglayan <ozancag@gmail.com>
2021-02-18 14:56:57 +0300

    metrics/helpers: remove unused function

--- sacrebleu/metrics/helpers.py
+++ sacrebleu/metrics/helpers.py
@@ -27,19 +27,3 @@ def extract_char_ngrams(line: str, n: int) -> Counter:
     :return: a dictionary containing ngrams and counts
     """
     return Counter([line[i:i + n] for i in range(len(line) - n + 1)])
-
-
-def extract_all_char_ngrams(line: str, min_order: int, max_order: int) -> Counter:
-    """Extracts all character ngrams (min_order <= n <= max_order) from a sentence.
-
-    :param line: A segment containing a sequence of words
-    :param min_order: Minimum n-gram length
-    :param max_order: Maximum n-gram length
-    :return: a dictionary containing ngrams and counts
-    """
-    ngrams = []
-
-    for n in range(min_order, max_order + 1):
-        ngrams.append(extract_char_ngrams(line, n))
-
-    return ngrams
