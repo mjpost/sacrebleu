4911efc88d9327089a1a2fb34d7000e75faf9cc3
Ozan Caglayan <ozancag@gmail.com>
2021-02-19 16:10:24 +0300

    helpers: change annotations, return type

--- sacrebleu/metrics/helpers.py
+++ sacrebleu/metrics/helpers.py
@@ -1,23 +1,23 @@
 from collections import Counter, defaultdict
-from typing import List
+from typing import List, Tuple, DefaultDict
 
 
-def extract_word_ngrams(tokens: List[str], min_order: int, max_order: int) -> Counter:
+def extract_word_ngrams(tokens: List[str], min_order: int, max_order: int) -> dict:
     """Extracts all ngrams (min_order <= n <= max_order) from a sentence.
 
     :param tokens: A list of tokens
     :param min_order: Minimum n-gram length
     :param max_order: Maximum n-gram length
-    :return: a tuple of (n-gram count dictionary, hypothesis length)
+    :return: a count dictionary
     """
 
-    ngrams = defaultdict(int)
+    ngrams: DefaultDict[tuple, str] = defaultdict(int)
 
     for n in range(min_order, max_order + 1):
         for i in range(0, len(tokens) - n + 1):
             ngrams[tuple(tokens[i: i + n])] += 1
 
-    return ngrams, len(tokens)
+    return dict(ngrams)
 
 
 def extract_char_ngrams(line: str, n: int) -> Counter:
