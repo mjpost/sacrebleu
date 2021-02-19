574711fbb028a0a3df04ce80802345e9aebeaf22
Ozan Caglayan <ozancag@gmail.com>
2021-02-19 21:56:13 +0300

    add sentence BLEU tests that cover smoothing, tokenizer, casing

---
+++ test/test_sentence_bleu.py
@@ -0,0 +1,45 @@
+import pytest
+import sacrebleu
+
+EPSILON = 1e-3
+
+
+# Example taken from #98
+REF = "producţia de zahăr brut se exprimă în zahăr alb;"
+SYS = "Producția de zahăr primă va fi exprimată în ceea ce privește zahărul alb;"
+
+test_cases = [
+    # change smoothing
+    ('exp', None, False, '13a', 8.493),
+    ('none', None, False, '13a', 0.0),
+    ('floor', None, False, '13a', 4.51688),    # defaults to 0.1
+    ('floor', 0.1, False, '13a', 4.51688),
+    ('floor', 0.5, False, '13a', 10.10),
+    ('add-k', None, False, '13a', 14.882),     # defaults to 1
+    ('add-k', 1, False, '13a', 14.882),
+    ('add-k', 2, False, '13a', 21.389),
+    # change tok
+    ('exp', None, False, 'none', 7.347),
+    ('exp', None, False, 'intl', 8.493),
+    ('exp', None, False, 'char', 40.8759),
+    # change case
+    ('exp', None, True, 'char', 42.0267),
+]
+
+
+@pytest.mark.parametrize("smooth_method, smooth_value, lowercase, tok, expected_score", test_cases)
+def test_compat_sentence_bleu(smooth_method, smooth_value, lowercase, tok, expected_score):
+    score = sacrebleu.compat.sentence_bleu(
+        SYS, [REF], smooth_method=smooth_method, smooth_value=smooth_value,
+        tokenize=tok,
+        lowercase=lowercase,
+        use_effective_order=True)
+    assert abs(score.score - expected_score) < EPSILON
+
+
+@pytest.mark.parametrize("smooth_method, smooth_value, lowercase, tok, expected_score", test_cases)
+def test_api_sentence_bleu(smooth_method, smooth_value, lowercase, tok, expected_score):
+    metric = sacrebleu.metrics.BLEU(
+        lowercase=lowercase, force=False, tokenize=tok,
+        smooth_method=smooth_method, smooth_value=smooth_value,)
+    metric.sentence_score(SYS, [REF], use_effective_order=True)
