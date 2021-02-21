47480a61b4860fc61af2e07ab03fd6cb695bc3e7
Ozan Caglayan <ozancag@gmail.com>
2021-02-22 00:23:20 +0300

    bump version to 2.0.0

--- sacrebleu/__init__.py
+++ sacrebleu/__init__.py
@@ -13,7 +13,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-__version__ = '1.5.0'
+__version__ = '2.0.0'
 __description__ = 'Hassle-free computation of shareable, comparable, and reproducible BLEU, chrF, and TER scores'
 
 
