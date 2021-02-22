0996b4a233ad07da9d1b5d8f3f6468d1f76d32a0
Ozan Caglayan <ozancag@gmail.com>
2021-02-22 11:49:22 +0300

    tokenizer_chrf: add chrF options to tokenizer, add caching

--- sacrebleu/tokenizers/tokenizer_chrf.py
+++ sacrebleu/tokenizers/tokenizer_chrf.py
@@ -1,4 +1,5 @@
 import re
+from functools import lru_cache
 
 from .tokenizer_base import BaseTokenizer
 
@@ -10,8 +11,20 @@ class TokenizerChrf(BaseTokenizer):
     def signature(self):
         return 'chrf'
 
-    def __init__(self):
-        self.whitespace_re = re.compile(r'\s+')
+    def __init__(self, lowercase: bool = False,
+                 include_whitespace: bool = False):
+        self.lowercase = lowercase
+        if include_whitespace:
+            self.whitespace_re = None
+        else:
+            self.whitespace_re = re.compile(r'\s+')
 
+    @lru_cache(maxsize=2**18)
     def __call__(self, sent: str) -> str:
-        return self.whitespace_re.sub('', sent).strip()
+        if self.lowercase:
+            sent = sent.lower()
+
+        if self.whitespace_re:
+            sent = self.whitespace_re.sub('', sent)
+
+        return sent.strip()
