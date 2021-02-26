294608a13e15e4cfce6dcaa01a3b996d7f7d229a
Ozan Caglayan <ozancag@gmail.com>
2021-02-26 21:33:11 +0300

    test_chrf: update tests

--- test/test_chrf.py
+++ test/test_chrf.py
@@ -14,34 +14,75 @@
 import pytest
 import sacrebleu
 
-EPSILON = 1e-8
+EPSILON = 1e-6
 
-test_cases = [(["Niemand hat die Absicht, eine Mauer zu errichten"], ["Niemand hat die Absicht, eine Mauer zu errichten"], 1.0),
-              (["abcdefg"], ["hijklmnop"], 0.0),
-              (["a"], ["a"], 1.0),
-              ([""], [""], 0.0),
-              ([""], ["reference"], 0.0),
-              (["a b c"], ["a b c"], 1.0),
-              (["a b c"], ["abc"], 1.0),
-              ([""], ["c"], 0.0),
-              (["a", "b"], ["a", "c"], 0.5),
-              (["source"], [""], 0.0),
-              (["aa"], ["ab"], 0.25),
-              ([" Die    Beziehung zwischen  Obama und Netanjahu ist nicht gerade  freundlich. "], ["Das Verhältnis zwischen Obama und Netanyahu ist nicht gerade freundschaftlich."], 0.64130269831561459),
-              ([" risk assessment must be made of those who are qualified and expertise in the sector - these are the scientists ."], ["risk assessment has to be undertaken by those who are qualified and expert in that area - that is the scientists ."], 0.63361730303214769)]
+test_sentence_level_chrf = [
+    (
+        'Co nás nejvíc trápí, protože lékaři si vybírají, kdo bude žít a kdo zemře.',
+        ['Nejvíce smutní jsme z toho, že musíme rozhodovat o tom, kdo bude žít a kdo zemře.'],
+        39.14078509,
+    ),
+    (
+        'Nebo prostě nemají vybavení, které by jim pomohlo, uvedli lékaři.',
+        ['A někdy nemáme ani potřebný materiál, abychom jim pomohli, popsali lékaři.'],
+        31.22557079,
+    ),
+    (
+        'Lapali po dechu, jejich životy skončily dřív, než skutečně začaly.',
+        ['Lapali po dechu a pak jejich život skončil - dřív, než skutečně mohl začít, připomněli.'],
+        57.15704367,
+    ),
+]
+
+
+test_cases = [
+    (["Niemand hat die Absicht, eine Mauer zu errichten"], ["Niemand hat die Absicht, eine Mauer zu errichten"], 1.0),
+    (["abcdefg"], ["hijklmnop"], 0.0),
+    (["a"], ["a"], 1.0),
+    (["a"], ["b"], 0.0),
+    ([""], ["reference"], 0.0),
+    (["a b c"], ["a b c"], 1.0),
+    (["a b c"], ["abc"], 1.0),
+    ([""], ["c"], 0.0),
+    (["a", "b"], ["a", "c"], 0.5),
+    #(["aa"], ["ab"], 0.25), # < 2.0.0, with effective order
+    (["aa"], ["ab"], 0.83333),  # >= 2.0.0 compatible with chrF++.py implementation
+    ([" Die    Beziehung zwischen  Obama und Netanjahu ist nicht gerade  freundlich. "],
+     ["Das Verhältnis zwischen Obama und Netanyahu ist nicht gerade freundschaftlich."], 0.64130269831561459),
+    ([" risk assessment must be made of those who are qualified and expertise in the sector - these are the scientists ."],
+     ["risk assessment has to be undertaken by those who are qualified and expert in that area - that is the scientists ."], 0.63361730303214769)]
+
 
 test_cases_keep_whitespace = [
-              (["Die Beziehung zwischen Obama und Netanjahu ist nicht gerade freundlich."], ["Das Verhältnis zwischen Obama und Netanyahu ist nicht gerade freundschaftlich."], 0.67348160629772402),
-              (["risk assessment must be made of those who are qualified and expertise in the sector - these are the scientists ."], ["risk assessment has to be undertaken by those who are qualified and expert in that area - that is the scientists ."], 0.652414427449)]
+    (
+        ["Die Beziehung zwischen Obama und Netanjahu ist nicht gerade freundlich."],
+        ["Das Verhältnis zwischen Obama und Netanyahu ist nicht gerade freundschaftlich."],
+        0.67348160629772402,
+    ),
+    (
+        ["risk assessment must be made of those who are qualified and expertise in the sector - these are the scientists ."],
+        ["risk assessment has to be undertaken by those who are qualified and expert in that area - that is the scientists ."],
+        0.652414427449,
+    ),
+]
 
 
 @pytest.mark.parametrize("hypotheses, references, expected_score", test_cases)
 def test_chrf(hypotheses, references, expected_score):
-    score = sacrebleu.corpus_chrf(hypotheses, [references], 6, 3).score
+    score = sacrebleu.corpus_chrf(
+        hypotheses, [references], char_order=6, word_order=0, beta=3).score
     assert abs(0.01 * score - expected_score) < EPSILON
 
 
 @pytest.mark.parametrize("hypotheses, references, expected_score", test_cases_keep_whitespace)
 def test_chrf_keep_whitespace(hypotheses, references, expected_score):
-    score = sacrebleu.corpus_chrf(hypotheses, [references], 6, 3, remove_whitespace=False).score
+    score = sacrebleu.corpus_chrf(
+        hypotheses, [references], char_order=6, word_order=0, beta=3,
+        remove_whitespace=False).score
     assert abs(0.01 * score - expected_score) < EPSILON
+
+
+@pytest.mark.parametrize("hypothesis, references, expected_score", test_sentence_level_chrf)
+def test_chrf_sentence_level(hypothesis, references, expected_score):
+    score = sacrebleu.sentence_chrf(hypothesis, references).score
+    assert abs(score - expected_score) < EPSILON
