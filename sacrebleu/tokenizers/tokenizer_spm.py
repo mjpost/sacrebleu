60aa8e73ec15d5da13e3a2acff6cf750480f8d9a
James Cross <james.henry.cross.iii@gmail.com>
2021-11-14 15:34:13 -0800

    rename signature spm-flores and store SPM model in SACRELEU_DIR

--- sacrebleu/tokenizers/tokenizer_spm.py
+++ sacrebleu/tokenizers/tokenizer_spm.py
@@ -4,12 +4,13 @@ import os
 import urllib.request
 
 from functools import lru_cache
+from ..utils import SACREBLEU_DIR
 from .tokenizer_base import BaseTokenizer
 
 
 class TokenizerSPM(BaseTokenizer):
     def signature(self):
-        return 'spm'
+        return 'spm-flores'
 
     def __init__(self):
         try:
@@ -20,10 +21,12 @@ class TokenizerSPM(BaseTokenizer):
                 '\n\n  pip install sentencepiece '
             )
         self.sp = spm.SentencePieceProcessor()
-        if not os.path.exists('sacrebleu_tokenizer_spm.model'):
+
+        tokenizer_path = os.path.join(SACREBLEU_DIR, "sacrebleu_tokenizer_spm.model")
+        if not os.path.exists(tokenizer_path):
             url = "https://dl.fbaipublicfiles.com/fairseq/models/flores/sacrebleu_tokenizer_spm.model"
-            urllib.request.urlretrieve(url, 'sacrebleu_tokenizer_spm.model')
-        self.sp.Load("sacrebleu_tokenizer_spm.model")
+            urllib.request.urlretrieve(url, tokenizer_path)
+        self.sp.Load(tokenizer_path)
 
     @lru_cache(maxsize=None)
     def __call__(self, line):
