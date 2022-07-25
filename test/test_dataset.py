4938071b81ac36193fe33a9855ef9ca9a5a2865b
Matt Post <mattpost@microsoft.com>
2022-07-25 10:13:46 -0400

    subsample datasets to download

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -32,7 +32,8 @@ def test_process_to_text():
 
     Ensure each field of specified language pair have the same length.
     """
-    for ds in dataset.DATASETS.values():
+    selected_datasets = random.choices(list(dataset.DATASETS.values()), k=10)
+    for ds in selected_datasets:
         if os.path.exists(ds._outdir):
             for filename in os.listdir(ds._outdir):
                 filepath = os.path.join(ds._outdir, filename)
