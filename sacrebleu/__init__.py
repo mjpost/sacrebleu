b0ad2cb29c87f51a6553b4d6caaff23d6bb7708c
Dmitry Ustalov <dmitry.ustalov@jetbrains.com>
2023-11-29 21:25:09 +0100

    Version bump to 2.3.3 (#253)

--- sacrebleu/__init__.py
+++ sacrebleu/__init__.py
@@ -14,7 +14,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-__version__ = '2.3.2'
+__version__ = '2.3.3'
 __description__ = 'Hassle-free computation of shareable, comparable, and reproducible BLEU, chrF, and TER scores'
 
 
